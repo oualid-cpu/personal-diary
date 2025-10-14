34c4158b0a99940443737a4f3633fa3442d9e0fe
Oualid <oualidchiki@gmail.com>
2025-10-14 12:47:58 +0200

    Clean React + Vite + TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-5xl font-bold text-cyan-400">Personal Diary</h1>
+      <p className="mt-4 text-lg text-pink-400">
+        It's Wroking!!!
+      </p>
+    </div>
+  );
+}
+
+export default App;
